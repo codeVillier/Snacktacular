7fd91ebbcf372e07530fc8490731f0673c34851b
codeVillier <odevill@icloud.com>
2026-06-22 15:31:53 +0200

    Initial Commit

---
+++ Snacktacular/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snacktacular
+//
+//  Created by Oscar De Villiers on 2026/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Oscar De Villiers on 2026/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
